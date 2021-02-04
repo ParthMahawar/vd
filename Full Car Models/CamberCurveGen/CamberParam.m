fe64529f078b861bdd866df32cd7c472163d6a99
lucasg7 <lucasg7@users.noreply.github.com>
2021-02-03 21:41:09 -0800

    Camber Commit

---
+++ Full Car Models/CamberCurveGen/CamberParam.m
@@ -0,0 +1,19 @@
+%Vehicle Width in inches
+C.front_width= 47;
+C.rear_width= 47;
+
+%Springrates in lbs/in
+C.front_spring_rate= 350;
+C.rear_spring_rate= 200;
+
+%Motion Ratio
+C.front_MR=.75;
+C.rear_MR=1;
+
+%ARB Rates
+C.front_ARB_rate=0;
+C.rear_ARB_rate=0.25;
+
+%ARB Motion Ratio
+C.front_ARB_mr
+C.rear_ARB_mr
