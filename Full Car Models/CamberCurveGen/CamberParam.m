4e0beba7599e40963948e1dc924ffb3fb181807a
lucasg7 <lucasg7@users.noreply.github.com>
2021-02-08 19:13:17 -0800

    Camber  Curve

--- Full Car Models/CamberCurveGen/CamberParam.m
+++ Full Car Models/CamberCurveGen/CamberParam.m
@@ -2,18 +2,35 @@
 C.front_width= 47;
 C.rear_width= 47;
 
-%Springrates in lbs/in
-C.front_spring_rate= 350;
-C.rear_spring_rate= 200;
+%Spring Roll Stiffness
+C.front_spring_stiff= 2860;
+C.rear_spring_stiff=2911;
 
-%Motion Ratio
-C.front_MR=.75;
-C.rear_MR=1;
+%ARB Roll Stiffness (Currently on Short)
+C.front_ARB_stiff=0;
+C.rear_ARB_stiff=1493;
 
-%ARB Rates
-C.front_ARB_rate=0;
-C.rear_ARB_rate=0.25;
+%Car Weight Distribution
+C.front_weight_distr=0.52;
+carParams.weight_dist = 0.54; % percentage of weight in rear
 
-%ARB Motion Ratio
-C.front_ARB_mr
-C.rear_ARB_mr
+%Total Car Weight
+carParams.mass = 179.2 + 5.9; % not including driver (395 lb)
+
+
+
+% %Springrates in lbs/in
+% C.front_spring_rate= 350;
+% C.rear_spring_rate= 200;
+% 
+% %Motion Ratio
+% C.front_MR=.75;
+% C.rear_MR=1;
+% 
+% %ARB Rates
+% C.front_ARB_rate=0;
+% C.rear_ARB_rate=0.25;
+% 
+% %ARB Motion Ratio
+% C.front_ARB_mr
+% C.rear_ARB_mr
