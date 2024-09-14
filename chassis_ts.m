3439798283c43bb11565a32a1354aa6be291fc10
John Yuan <jyuan27@berkeley.edu>
2024-09-14 01:19:56 -0700

    B25 WinGeo

---
+++ chassis_ts.m
@@ -0,0 +1,8 @@
+weight_dist = 0.512
+rchF = 4.7
+rchR = 3.5
+rsF = 2534 %in*lb/deg
+rsR = [4404 3896 3610 2911] %ARB short, med, long, off
+chassis_ts = [1000 1250 1500 1750 2000] * 8.85 %in*lb/deg
+lat_gs = [0.5 1 1.5]
+Wf = 
